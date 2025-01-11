3c5911069577b38b2a438eb19dbc0b6626c3605d
Zeinab <zeinabansarian6280@gmail.com>
2025-01-11 11:43:30 +0330

    change product cat page

--- js/productCat.js
+++ js/productCat.js
@@ -1,52 +1,34 @@
-let Productss = document.querySelectorAll('.swiper-slide > .Product')
-Productss.forEach(p=>{
-  console.log(p.querySelector('img'));
-  
-  gsap.to(p.querySelector('.bg-Color'),{
-      ease:"none",
-      height:0,
-    })
-})
-
-
-//productCat slider
-let productCat = new Swiper('.swiper-productCat',{
-    speed: 1000,
-    slidesPerView:2.5,
+let Productss = document.querySelectorAll(".swiper-slide > .Product");
+Productss.forEach(e => {
+  console.log(e.querySelector("img"));
+  gsap.to(e.querySelector(".bg-Color"), { ease: "none", height: 0 });
+});
+new Swiper(".swiper-productCat",
+  {
+    speed: 1e3,
+    slidesPerView: "auto",
     spaceBetween: 30,
     mousewheel: true,
-    grabCursor:true,
+    grabCursor: true,
     pagination: {
-        el: '.swiper-pagination',
-              clickable: true,
-          renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + '<span class="line"></span>' + '</span>';
-          },
-      },
-    on:{
-        init(e){
-            let SlidesLength = e.slides.length
-            e.slides.forEach(r=>{
-            gsap.to(r.querySelectorAll('.swiper-slide .Title h2'),{
-             y:0,
-             opacity:1
-              })
-            })
-            let slides = document.querySelectorAll('.swiper-slide')
-            let i = 1
-            slides.forEach((slide)=>{
-                slide.setAttribute('data-index',i)
-                let Current = slide.querySelector('.Current')
-                Current.innerText=`0${i}`
-                i++
-                if(i>=SlidesLength){
-                  return
-                }
-            })
-        },
-        
-        slideChangeTransitionStart(e){
-
-        }
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (e, r) {
+        return '<span class="' + r + '"><span class="line"></span></span>';
+      }
+    }, on: {
+      init(e) {
+        e.slides.length;
+        e.slides.forEach(e => {
+          gsap.to(e.querySelectorAll(".swiper-slide .Title h2"), { y: 0, opacity: 1 });
+        });
+        let r = document.querySelectorAll(".swiper-slide");
+        let t = 1;
+        r.forEach(e => {
+          e.setAttribute("data-index", t);
+          e.querySelector(".Current").innerText = `0${t}`;
+          t++;
+        });
+      }, slideChangeTransitionStart(e) { }
     }
-})
+  });
